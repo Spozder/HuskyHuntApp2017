ca2c69d40794aa02fa310dd3a3f2a909f9a182fc
Spozder <stpoz@live.com>
2017-10-25 20:55:02 -0400

    Initial Redux Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { INCREMENT, DECREMENT, RESET } from '../../state/counter';
 import { NavController } from 'ionic-angular';
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  counter: Observable<number>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private store: Store<AppState>) {
+    this.counter = store.select('counter');
+  }
 
+  increment() {
+    this.store.dispatch({type: INCREMENT});
+  }
+
+  decrement() {
+    this.store.dispatch({type: DECREMENT});
+  }
+
+  reset() {
+    this.store.dispatch({type: RESET});
   }
 
 }
